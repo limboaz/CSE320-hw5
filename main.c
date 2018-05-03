d460fcb62655e3a18c74cd486c092b6d86fab646
yinuozh <yinuzhang@cs.stonybrook.edu>
2018-05-03 05:10:44 -0700

    not much going on

--- main.c
+++ main.c
@@ -1,19 +1,54 @@
 #include <stdio.h>
-#include <sthlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+
+pthread_t id[4];	//process list
+
+//A struct of page table for each process. 
+typedef struct {
+	pthread_t tid;	//thread ID
+	//need a list size. Addresses are represented by uint32_t integers. 
+}pt;
+
+
+//functions need to be created
+//return 0 on success
+
+void *thread(void *vargp);
+void *cse320_malloc(int n);	
+void *cse320_virt_to_phys(void *vir);
+int kill_x(pthread_t tid);
+void list();
+void mem_x(pthread_t tid);
+int allocate(pthread_t tid);
+int read_xy(pthread_t tid, void *p);
+
+
+int create(){
+	int i;
+	int added = 0;
+	pthread_t tid;
+	pthread_create(&tid, NULL, thread, NULL);
+	for ( i = 0; i < 4; i++){
+		if ( id[i] < 1 ){
+			id[i] = tid;
+			added = 1;
+			break;
+		}
+	}
+	return added;
+}
+
+void *thread(void *vargp){
+	//when creating a thread, create a corresponding page table. 
+	
+}
 
 
 void main(){
-	fputs("Please select from the following option\n
-			create\n
-			kill X\n
-			list\n
-			mem X\n
-			allocate X\n
-			read X Y\n
-			write X Y Z\n
-			exit\n", stdout);
+	fputs("Please select from the following option\ncreate\nkill X\nlist\nmem X\nallocate X\nread X Y\nwrite X Y Z\nexit\n", stdout);
 	char *args[5];
 	char cmd[127];
 	while(1){
